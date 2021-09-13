7e2bd4d1f921df7f2ff0838b6fd9d041d1df46bb
mike-duvall <mike.a.duvall@gmail.com>
2021-09-13 07:22:23 -0500

    Initial monogame generated solution and project, plus .gitignore

---
+++ mike-and-conquer-monogame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace mike_and_conquer_monogame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
