b11172446dc2f167c9b694b9fd7acd0c8e6717f4
mike-duvall <mike.a.duvall@gmail.com>
2021-09-27 07:04:46 -0500

    Added basic minigunner endpoint

--- mike-and-conquer-simulation/rest/controller/MinigunnersController.cs
+++ mike-and-conquer-simulation/rest/controller/MinigunnersController.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using mike_and_conquer_simulation.rest.domain;
 // using WeatherForecast = mike_and_conquer_simulation.rest.domain.WeatherForecast;
-using Minigunner = mike_and_conquer_simulation.rest.domain.Minigunner;
 
 namespace mike_and_conquer_simulation.rest.controller
 {
@@ -12,9 +13,6 @@ namespace mike_and_conquer_simulation.rest.controller
     [Route("[controller]")]
     public class MinigunnersController : ControllerBase
     {
-
-
-
 
 
         private static readonly string[] Summaries = new[]
@@ -30,18 +28,58 @@ namespace mike_and_conquer_simulation.rest.controller
         }
 
         [HttpGet]
-        public IEnumerable<Minigunner> Get()
+        public IEnumerable<RestMinigunner> Get()
         {
             _logger.LogInformation("This is some test logging from the simulation.  And, Mike is cool");
             _logger.LogWarning("This is some test logging from the simulation.  And, Mike is cool");
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new Minigunner
+            int baseX = 1;
+            int baseY = 1;
+            return Enumerable.Range(1, 5).Select(index => new RestMinigunner
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                X = baseX++,
+                Y= baseY++
+
+                // Date = DateTime.Now.AddDays(index),
+                // TemperatureC = rng.Next(-20, 55),
+                // Summary = Summaries[rng.Next(Summaries.Length)]
+
             })
             .ToArray();
         }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<RestMinigunner> PostProduct([FromBody] RestMinigunner incomingRestMinigunner)
+        {
+            try
+            {
+
+                // var result = CreatedAtAction(nameof(GetById), new { id = 5 },minigunner);
+                // var s = minigunner.ToString();
+                // var t = minigunner.GetType();
+                // return new CreatedResult($"/products/{5}", minigunner);
+
+                RestMinigunner createdRestMinigunner = new RestMinigunner();
+                createdRestMinigunner.X = 100;
+                createdRestMinigunner.Y = 200;
+                createdRestMinigunner.ID = 2;
+
+                
+
+                return new CreatedResult($"/minigunners/{createdRestMinigunner.ID}", createdRestMinigunner);
+
+
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning(e, "Unable to POST minigunner.");
+
+                return ValidationProblem(e.Message);
+            }
+        }
+
     }
 }
