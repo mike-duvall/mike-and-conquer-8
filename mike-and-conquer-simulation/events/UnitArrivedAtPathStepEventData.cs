27d8472646786b5554a70751bd094ac9ab70eec3
mike-duvall <mike.a.duvall@gmail.com>
2022-06-21 06:44:18 -0500

    Tweaks to publishing path events

---
+++ mike-and-conquer-simulation/events/UnitArrivedAtPathStepEventData.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+
+
+namespace mike_and_conquer_simulation.events
+{
+    class UnitArrivedAtPathStepEventData
+    {
+
+        public const string EventType = "UnitArrivedAtPathStep";
+
+
+        public int UnitId { get; }
+        public long Timestamp { get; }
+
+        public PathStep PathStep { get;  }
+
+
+        public UnitArrivedAtPathStepEventData(int unitId, PathStep pathStep)
+        {
+            this.UnitId = unitId;
+            this.Timestamp = DateTime.Now.Ticks;
+            this.PathStep = pathStep;
+
+        }
+
+    }
+}
